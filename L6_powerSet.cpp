7e4d39a1199d5aafb31a9d269d4d52089ce5a557
Sukanya-namburi <69515025+Sukanya-namburi@users.noreply.github.com>
2022-04-21 22:50:51 +0530

    Update L6_powerSet.cpp

--- L6_powerSet.cpp
+++ L6_powerSet.cpp
@@ -27,3 +27,31 @@ int main(){
         cout<<endl;
     }
 }
+
+
+//printing subsequences using recursion
+#include<bits/stdc++.h>
+using namespace std;
+void printSub(int ind,vector<int>&ds,int n,int arr[]){
+    if(ind==n){
+        for(int i=0;i<ds.size();i++){
+            cout<<ds[i]<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    ds.push_back(arr[ind]);
+    printSub(ind+1,ds,n,arr);
+    ds.pop_back();
+    printSub(ind+1,ds,n,arr);
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>ds;
+    printSub(0,ds,n,arr);
+}
