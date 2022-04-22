349d5ac3a1d18a6fa9b9e3163faadadfbf1475a2
Sukanya-namburi <69515025+Sukanya-namburi@users.noreply.github.com>
2022-04-22 10:44:56 +0530

    Update L7_patterns.cpp

--- L7_patterns.cpp
+++ L7_patterns.cpp
@@ -31,3 +31,68 @@ int main(){
     printSub(0,ds,0,Target,n,arr);
 }
 
+
+//printing any one subsequence whose sum is K
+#include<bits/stdc++.h>
+using namespace std;
+bool printSub(int ind,vector<int>&ds,int sum,int Target,int n,int arr[]){
+    if(ind==n){
+        if(sum==Target){
+            for(int i=0;i<ds.size();i++){
+                cout<<ds[i]<<" ";
+            }
+            cout<<endl;
+            return true;
+        }
+        else return false;
+    }
+    ds.push_back(arr[ind]);
+    sum+=arr[ind];
+    if(printSub(ind+1,ds,sum,Target,n,arr)==true) return true;
+    ds.pop_back();
+    sum-=arr[ind];
+    if(printSub(ind+1,ds,sum,Target,n,arr)==true) return true;
+    return false;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int Target;
+    cin>>Target;
+    vector<int>ds;
+    printSub(0,ds,0,Target,n,arr);
+}
+
+//printing any one subsequence whose sum is K
+#include<bits/stdc++.h>
+using namespace std;
+int printSub(int ind,vector<int>&ds,int sum,int Target,int n,int arr[]){
+    if(ind==n){
+        if(sum==Target) return 1;
+        else return 0;
+    }
+  
+    sum+=arr[ind];
+    int l=printSub(ind+1,ds,sum,Target,n,arr);
+    ds.pop_back();
+    sum-=arr[ind];
+    int r=printSub(ind+1,ds,sum,Target,n,arr);
+    return l+r;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int Target;
+    cin>>Target;
+    vector<int>ds;
+    cout<<printSub(0,ds,0,Target,n,arr);
+}
+
